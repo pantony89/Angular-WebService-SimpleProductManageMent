19fc0707af2c689f68a5c0828a180553d91fa38d
pantony89 <pantony89@gmail.com>
2017-06-12 18:17:21 -0400

    Routing

--- ProductManagement.Client/app/app.js
+++ ProductManagement.Client/app/app.js
@@ -1,6 +1,32 @@
 ﻿(function () {
     "use strict";
 
-    debugger;
-    var app = angular.module("productManagement", ["common.services", "productResourceMock"]);
+  
+    var app = angular.module("productManagement", ["common.services","ui.router",
+                             "productResourceMock"]);
+
+    app.config(["$stateProvider",
+                "$urlRouterProvider",
+        function($stateProvider, $urlRouterProvider){
+            $urlRouterProvider.otherwise("/");
+
+            $stateProvider
+                .state("home", {
+                    url: "/",
+                    templateUrl: "app/welcomeView.html"
+                })
+                // Products
+                .state("productList", {
+                    url: "/products",
+                    templateUrl: "app/products/productListView.html",
+                    controller: "ProductListCtrl as vm"
+                })
+                .state("productEdit", {
+                    url: "/products/edit/:productId",
+                    templateUrl: "app/products/productEditView.html",
+                    controller: "ProductEditCtrl as vm"
+                })
+
+        }]
+    );
 }());
